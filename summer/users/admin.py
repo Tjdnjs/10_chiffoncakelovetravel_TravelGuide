f10e8c3324f1eda1f5340ccbc45555568dadced4
serasong <gahyunsong@hufs.ac.kr>
2022-07-24 04:40:11 +0900

    --

--- summer/users/admin.py
+++ summer/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Review
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname",)}),)
+
+admin.site.register(Review)
